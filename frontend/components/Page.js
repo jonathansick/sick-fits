429193e282cdd216e2e01d34db8f1d418d607a59
Jonathan Sick <jsick@lsst.org>
2018-11-17 22:38:56 -0700

    Create Header and Nav components
    This Header is used by the Page component so it appears on every page.

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Header from './Header';
 
 export default class Page extends Component {
   render() {
     return (
       <div>
-        <p>Hey I'm on every page.</p>
-        { this.props.children }
+        <Header />
+        {this.props.children}
       </div>
-    )
+    );
   }
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -13,7 +13,7 @@ class MyApp extends App {
           <Component />
         </Page>
       </Container>
-    )
+    );
   }
 }
 
