5bce23c9132b25e13a03030d78c55c9464e8d692
Jonathan Sick <jsick@lsst.org>
2018-11-19 22:41:27 -0700

    Set global styling via injectGlobal
    From styled components.
    
    Set colors, fonts, sizes, and resets this way. Everything else is styled
    on the component itself.

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
 
@@ -26,6 +26,34 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+// Set global styles with Styled Components
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px; // base 10px, then use rems
+  }
+  *, *:before, *:after {
+    box-sizing: inherit
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem; // 15px
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black}; // access stuff from the theme like this
+  }
 `;
 
 export default class Page extends Component {
