cbbfff96159ab45d372b4361f653c2e397a4d5f9
Jonathan Sick <jsick@lsst.org>
2018-11-22 10:48:22 -0700

    Show how to use forwardTo in Query
    This is useful if we don't have custom logic and just want to reflect an
    API from prisma.graphql.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,10 +1,14 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    console.log('Getting items');
-    // query.items() comes from prisma.graphql
-    const items = await ctx.db.query.items();
-    return items;
-  },
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting items');
+  //   // query.items() comes from prisma.graphql
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+  // Forward items Yoga directly to items Query in prisma.graphql
+  items: forwardTo('db'),
 };
 
 module.exports = Query;
