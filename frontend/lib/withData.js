df0acab79b6ecb45f9eb43bea535299e5102e90a
Jonathan Sick <jsick@lsst.org>
2018-12-01 23:42:49 -0700

    Wrap page component with ApolloProvider
    This lets our pages use apollo to access the GraphQL backend.

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,10 +1,17 @@
+// Boilerplate that creates the withApollo higher-order React/Next.js component
+// with the Apollo client instance in it. This gets used as a wrapper for the
+// app in /pages/_app.js.
+
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
 
+// Create Apollo client
 function createClient({ headers }) {
   return new ApolloClient({
+    // Endpoint URI for backend API
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    // Middleware that includes headers and credentials with requests
     request: operation => {
       operation.setContext({
         fetchOptions: {
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,39 @@
 // Custom Next.js App component
 
+import { ApolloProvider } from 'react-apollo';
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      // Crawls all pages to get their initial props so they're available from
+      // the app component.
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    // apollo props comes from withData high order component
+    // pageProps comes from getInitialProps
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+// Wrap app in withData high-order-component that provides the apollo pop.
+export default withData(MyApp);
